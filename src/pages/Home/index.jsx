3d8c8cd055b34a7ca3b9147ac4e2def8e1f5f553
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-12-01 14:17:32 -0300

    add style to list in home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import './styles.css';
 
 export function Home() {
   return (
-    <div>
+    <div className="container">
       <h1>Lista de presença</h1>
       <input type="text" placeholder="Digite um nome"/>
       <button type="button">Adicionar</button>
