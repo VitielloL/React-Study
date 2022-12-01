d9d56089f2e2f1ae416dcf47b12f57f8d85ac83f
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-12-01 15:50:48 -0300

    add useEffect name avartar with api github

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import { Card } from '../../components/Card';
 
@@ -6,6 +6,7 @@ export function Home() {
   const [clientName, setClientName] = useState('valor inicia do useState');
   // clientName = nome do estado, SetClientName = função que atualiza o estado
   const [namesOnList,setNamesOnList] = useState([])
+  const [user, setUser] = useState({ name:'', avatar: '' })
 
   // toda vez que handleAddNameOnList for chamada, irá criar um novo objeto com 2 propriedades, 
   // o nome que é pego do estado que armazena o conteudo atual do input e o time que pega do horario atual
@@ -21,11 +22,48 @@ export function Home() {
     }
     // prevState para recuperar o estado anterior, precisa ser passado com o operador spread para que ele recebe o anterior 
     setNamesOnList(prevState => [...prevState, newNameOnList]);
-  }
+  } 
+
+  useEffect(() => {
+    // corpo do useEffect, ações para ser executada, use effect é executado automaticamente junto a renderização da interface
+    // useEffect nao pode ser assincrono
+
+    // ----------------------------------------------
+    // forma de fazer assincrona:
+      // async function fetchData() {
+      //   const response = await fetch('https://api.github.com/users/VitielloL')
+      //   const data = await response.json
+      //   setUser({
+      //     name: data.name,
+      //     avatar: data.avatar_url
+      //   })
+      // }
+      // fetchData()
+    // -----------------------------------------------
+    
+    // fetch utilizado para requisições http
+    fetch('https://api.github.com/users/VitielloL')
+      .then(response => response.json())
+      .then(data => {
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url
+        })
+      })
+      .catch(error => console.log(error))
+  }, [])
+
 
   return (
     <div className="container">
-      <h1>Lista de presença</h1>
+      <header>
+        <h1>Lista de presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil"/>
+        </div>
+      </header>
+
       <h5>Esta sendo digitado: {clientName}</h5>
       <input 
         type="text" 
