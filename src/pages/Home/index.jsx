7b6e6d2b62d6acbc551c8918f91d00c536de2391
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-12-01 15:15:48 -0300

    add functionality correctly with comments

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,25 @@ import { Card } from '../../components/Card';
 
 export function Home() {
   const [clientName, setClientName] = useState('valor inicia do useState');
-  
+  // clientName = nome do estado, SetClientName = função que atualiza o estado
+  const [namesOnList,setNamesOnList] = useState([])
+
+  // toda vez que handleAddNameOnList for chamada, irá criar um novo objeto com 2 propriedades, 
+  // o nome que é pego do estado que armazena o conteudo atual do input e o time que pega do horario atual
+  // depois que monta o objeto adiciona ele no estado setNameOnList
+  function handleAddNameOnList(){
+    const newNameOnList = {
+      name: clientName,
+      time: new Date().toLocaleDateString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    }
+    // prevState para recuperar o estado anterior, precisa ser passado com o operador spread para que ele recebe o anterior 
+    setNamesOnList(prevState => [...prevState, newNameOnList]);
+  }
+
   return (
     <div className="container">
       <h1>Lista de presença</h1>
@@ -12,13 +30,17 @@ export function Home() {
       <input 
         type="text" 
         placeholder="Digite um nome"
+        // pega o valor digitado no input
         onChange={e => setClientName(e.target.value)}
       />
-      <button type="button">Adicionar</button>
+      <button type="button" onClick={handleAddNameOnList}>
+        Adicionar
+      </button>
 
-      <Card name="Lucas" time="18:05"/>
-      <Card name="Julia" time="15:35"/>
-
+      {
+        // passa um map no vetor buscando as propriedades solicitadas
+        namesOnList.map(nameOnList => <Card name={nameOnList.name} time={nameOnList.time}/>)
+      }
     </div>
   )
 }
