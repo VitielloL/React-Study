5c06ae7999e576018c8a80a2f2770fb069be5144
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-12-01 14:50:40 -0300

    add use state

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,19 @@
+import React, { useState } from 'react';
 import './style.css';
 import { Card } from '../../components/Card';
 
 export function Home() {
+  const [clientName, setClientName] = useState('valor inicia do useState');
+  
   return (
     <div className="container">
       <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite um nome"/>
+      <h5>Esta sendo digitado: {clientName}</h5>
+      <input 
+        type="text" 
+        placeholder="Digite um nome"
+        onChange={e => setClientName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
 
       <Card name="Lucas" time="18:05"/>
